eac03556248c1f850da36cb798660eec8858eb4d
Rune Madsen <runemadsen@mac.com>
2015-06-23 12:38:02 -0700

    Initial commit

---
+++ BUYPaymentButtonSample/BUYPaymentButtonSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BUYPaymentButtonSample
+//
+//  Created by Rune Madsen on 2015-06-12.
+//  Copyright (c) 2015 Shopify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BUYPaymentButtonSample/BUYPaymentButtonSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BUYPaymentButtonSample
+//
+//  Created by Rune Madsen on 2015-06-12.
+//  Copyright (c) 2015 Shopify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
