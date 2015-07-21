2898f7de6eab537604b9562405bf3ba957417484
Jonathan Baker <jonathan@jonathanbaker.me>
2015-07-21 15:39:03 -0400

    Add framework target.

--- BUYPaymentButton/BUYPaymentButton.h
+++ BUYPaymentButton/BUYPaymentButton.h
@@ -6,6 +6,13 @@
 //  Copyright (c) 2015 Shopify Inc. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+
+#if defined(BUYFRAMEWORK_TARGET) && BUYFRAMEWORK_TARGET
+FOUNDATION_EXPORT double BUYPaymentButtonVersionNumber;
+FOUNDATION_EXPORT const unsigned char BUYPaymentButtonVersionString[];
+#endif
+
 typedef NS_ENUM(NSInteger, BUYPaymentButtonStyle) {
     BUYPaymentButtonStyleWhite = 0,
     BUYPaymentButtonStyleWhiteOutline,
